5658441a88f61f9460692c576ef72d0a117dbf2c
soren <me@soren.dev>
2019-06-09 03:34:26 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import moment from 'moment';
+import uuid from 'uuid';
 
-function App() {
+const App = () => {
+  const [timestamps, setTimestamps] = useState([]);
+
+  const getTime = (e) => {
+    e.preventDefault();
+
+    const timeNow = new Date();
+    const currentMoment = moment(timeNow);
+
+    setTimestamps([
+      ...timestamps,
+      {
+        time: currentMoment.format("h:mm:ss a - ddd, MMM Do YYYY"),
+        key: uuid()
+      }
+    ])
+  }
+
+  const removeEntry = (x) => {
+    setTimestamps(timestamps.filter(entry => entry.key !== x))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>test</h1>
+      <form onSubmit={getTime}>
+        <button>Add Time</button>
+      </form>
+      <ul>
+        {timestamps.map(({ key, time }) => (
+          <li key={key}>
+            {time}
+            <button onClick={() => removeEntry(key)}>
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
+
 export default App;
+
+// as a user, i want to click a button to show the time
+// as a user, i want to click a button more than once and show each time in a list
+// as a user, i want to delete individual items in a list
+
+// map(timeStamp, index) => <li>{timeStamp}</li>
+// map(timeStamp) => <li>{timeStamp.timeStamp}</li>
+
